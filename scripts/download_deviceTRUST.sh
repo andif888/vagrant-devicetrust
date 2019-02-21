9fade738f89b317a37ef8f1409b2bd76099688de
andif888 <andif@snowbay.de>
2019-02-21 13:54:49 +0100

    Updated deviceTRUST 19.1.100

--- scripts/download_deviceTRUST.sh
+++ scripts/download_deviceTRUST.sh
@@ -8,6 +8,6 @@ if [ -f "/tmp/deviceTRUST.zip" ]; then
   rm -f /tmp/deviceTRUST.zip
 fi
 
-wget --progress=bar:force -O /tmp/deviceTRUST.zip 'https://minio.prianto.com/devicetrust/deviceTRUST-19.1.15.0.zip'
+wget --progress=bar:force -O /tmp/deviceTRUST.zip 'https://minio.prianto.com/devicetrust/deviceTRUST-19.1.100.zip'
 
 unzip /tmp/deviceTRUST.zip -d /tmp/deviceTRUST/
